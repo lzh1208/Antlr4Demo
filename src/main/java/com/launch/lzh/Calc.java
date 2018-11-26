e5addf790d858e1f046c57f49e77e9585606bd0c
liuzehui <zehui.liu@cnlaunch.com>
2018-11-26 10:49:08 +0800

    计算器例子实现

---
+++ src/main/java/com/launch/lzh/Calc.java
@@ -0,0 +1,30 @@
+package com.launch.lzh;
+
+import com.launch.lzh.out2.LabeledExprLexer;
+import com.launch.lzh.out2.LabeledExprParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * Created by liuzehui on 2018/11/26.
+ */
+public class Calc {
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if ( args.length>0 ) inputFile = args[0];
+        InputStream is = System.in;
+        if ( inputFile!=null ) is = new FileInputStream(inputFile);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        LabeledExprLexer lexer = new LabeledExprLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LabeledExprParser parser = new LabeledExprParser(tokens);
+        ParseTree tree = parser.prog(); // parse
+
+        EvalVisitor eval = new EvalVisitor();
+        eval.visit(tree);
+    }
+}
---
+++ src/main/java/com/launch/lzh/testParser.java
@@ -0,0 +1,46 @@
+package com.launch.lzh;
+
+import com.launch.lzh.out.ExprLexer;
+import com.launch.lzh.out.ExprParser;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+/**
+ * Created by liuzehui on 2018/11/23.
+ */
+public class testParser {
+    public static void run(String expr) throws Exception {
+
+        //对每一个输入的字符串，构造一个 ANTLRStringStream 流 in
+        ANTLRInputStream in = new ANTLRInputStream(expr);
+
+        //用 in 构造词法分析器 lexer，词法分析的作用是产生记号
+        ExprLexer lexer = new ExprLexer(in);
+
+        //用词法分析器 lexer 构造一个记号流 tokens
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        //再使用 tokens 构造语法分析器 parser,至此已经完成词法分析和语法分析的准备工作
+        ExprParser parser = new ExprParser(tokens);
+
+        //最终调用语法分析器的规则 prog，完成对表达式的验证
+        ParseTree tree=parser.prog();
+        System.out.println(tree.toStringTree(parser));
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String[] testStr = {
+                "2\n",
+                "a+b+3\n",
+                "(a-b)+3\n",
+                "a+(b*3\n"
+        };
+
+        for (String s : testStr) {
+            System.out.println("Input expr:" + s);
+            run(s);
+        }
+    }
+}
